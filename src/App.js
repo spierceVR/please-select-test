dc99937f23e6c4f361c512fc16a2039e0aaffb32
solom <1nepthys1@gmail.com>
2022-01-01 02:19:51 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
+      <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+          crossorigin="anonymous"></link>
+      </head>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> Are you authorized to work at DN? 
+          <span class="required"> *</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <select class="form-select btn btn-secondary dropdown-toggle" aria-label="Default select example">
+          <option selected>Please select</option>
+          <option value="1">Test</option>
+        </select>
       </header>
     </div>
   );
